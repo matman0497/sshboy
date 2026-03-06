74b9f2f042ce8e56b08abecbec5c655104983df1
Mattia Mancina <mattia.mancina@additive.eu>
2026-03-06 17:36:26 +0100

    feat: add ping command

---
+++ cmd/ping.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"mattiamancina/sshboy/config"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(pingCommand)
+}
+
+var pingCommand = &cobra.Command{
+	Use:   "ping [server name]",
+	Short: "Ping a server",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		server := config.GetServer(args[0])
+
+		if server == nil {
+			fmt.Printf("Server %s not found\n", args[0])
+			os.Exit(1)
+		}
+
+		sshCmd := exec.Command("ping", server.Host)
+
+		sshCmd.Stdin = os.Stdin
+		sshCmd.Stdout = os.Stdout
+		sshCmd.Stderr = os.Stderr
+
+		err := sshCmd.Run()
+		if err != nil {
+			panic(err)
+		}
+
+	},
+}
